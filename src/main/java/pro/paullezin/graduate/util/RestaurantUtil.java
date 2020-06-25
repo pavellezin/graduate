393833a480be74f319ef47edbf59e09c89917fc6
Pavel Lezin <pavellezin@yandex.ru>
2020-06-25 13:41:31 +0500

    dev 1_3 add_utils

---
+++ src/main/java/pro/paullezin/graduate/util/RestaurantUtil.java
@@ -0,0 +1,31 @@
+package pro.paullezin.graduate.util;
+
+import pro.paullezin.graduate.model.Restaurant;
+import pro.paullezin.graduate.to.RestaurantTo;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+public class RestaurantUtil {
+
+    private RestaurantUtil() {
+    }
+
+    public static List<RestaurantTo> getTos(Collection<Restaurant> restaurants, LocalDateTime dateTime) {
+        return null;
+    }
+
+    private static RestaurantTo createTo(Restaurant restaurant) {
+        return null;
+    }
+
+    public static List<RestaurantTo> filteredByStreams(Collection<Restaurant> restaurants, LocalDateTime dateTime) {
+        return null;
+    }
+
+    public static List<RestaurantTo> getFilteredTos(Collection<Restaurant> restaurants, LocalDateTime dateTime) {
+        return null;
+    }
+}
+
