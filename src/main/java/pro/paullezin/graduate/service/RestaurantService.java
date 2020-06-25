73998717754e63aab2eb2afc4b7ff89fcebeaa5e
Pavel Lezin <pavellezin@yandex.ru>
2020-06-25 17:25:06 +0500

    dev 1_4 add_restaurant_service

---
+++ src/main/java/pro/paullezin/graduate/service/RestaurantService.java
@@ -0,0 +1,42 @@
+package pro.paullezin.graduate.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import pro.paullezin.graduate.model.Restaurant;
+import pro.paullezin.graduate.repository.RestaurantRepository;
+
+import java.util.List;
+
+import static pro.paullezin.graduate.util.ValidationUtil.checkNotFoundWithId;
+
+//@Service
+public class RestaurantService {
+
+    private final RestaurantRepository repository;
+
+    public RestaurantService(RestaurantRepository repository) {
+        this.repository = repository;
+    }
+
+    public Restaurant get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public List<Restaurant> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(Restaurant restaurant) {
+        Assert.notNull(restaurant, "restaurant must not be null");
+        checkNotFoundWithId(repository.save(restaurant), restaurant.getId());
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        Assert.notNull(restaurant, "restaurant must not be null");
+        return repository.save(restaurant);
+    }
+}
