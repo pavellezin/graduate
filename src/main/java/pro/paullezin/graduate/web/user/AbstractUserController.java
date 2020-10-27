9ecb642575b2ba34d7a1971e4576e590143be2e4
Pavel Lezin <pavellezin@yandex.ru>
2020-10-27 14:35:35 +0500

    dev 3_5 remove_user_service_layer

--- src/main/java/pro/paullezin/graduate/web/user/AbstractUserController.java
+++ src/main/java/pro/paullezin/graduate/web/user/AbstractUserController.java
@@ -3,50 +3,54 @@ package pro.paullezin.graduate.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import pro.paullezin.graduate.model.User;
-import pro.paullezin.graduate.service.UserService;
+import pro.paullezin.graduate.repository.UserRepository;
 
 import java.util.List;
 
-import static pro.paullezin.graduate.util.ValidationUtil.assureIdConsistent;
-import static pro.paullezin.graduate.util.ValidationUtil.checkNew;
+import static pro.paullezin.graduate.util.ValidationUtil.*;
 
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+
     @Autowired
-    private UserService service;
+    private UserRepository repository;
 
     public List<User> getAll() {
         log.info("getAll");
-        return service.getAll();
+        return repository.getAll();
     }
 
     public User get(int id) {
         log.info("get {}", id);
-        return service.get(id);
+        return checkNotFoundWithId(repository.get(id), id);
     }
 
     public User create(User user) {
         log.info("create {}", user);
+        Assert.notNull(user, "user must not be null");
         checkNew(user);
-        return service.create(user);
+        return repository.save(user);
     }
 
     public void delete(int id) {
         log.info("delete {}", id);
-        service.delete(id);
+        checkNotFoundWithId(repository.delete(id), id);
     }
 
     public void update(User user, int id) {
         log.info("update {} with id={}", user, id);
+        Assert.notNull(user, "user must not be null");
         assureIdConsistent(user, id);
-        service.update(user);
+        checkNotFoundWithId(repository.save(user), user.getId());
     }
 
     public User getByMail(String email) {
         log.info("getByEmail {}", email);
-        return service.getByEmail(email);
+        Assert.notNull(email, "email must not be null");
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,7 +3,8 @@ DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-('user', 'user@paullezin.pro', 'user'),
+('user1', 'user1@paullezin.pro', 'user1'),
+('user2', 'user2@paullezin.pro', 'user2'),
 ('admin', 'admin@paullezin.pro', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
