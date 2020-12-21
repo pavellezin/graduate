6d71143b143704c2c01ae91c2615cf3babfca23a
Pavel Lezin <pavellezin@yandex.ru>
2020-12-21 11:58:39 +0500

    dev 7_3 add_rest_for_restaurants

--- src/main/java/pro/paullezin/graduate/web/restaurant/RestaurantRestController.java
+++ src/main/java/pro/paullezin/graduate/web/restaurant/RestaurantRestController.java
@@ -2,9 +2,10 @@ package pro.paullezin.graduate.web.restaurant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
 import pro.paullezin.graduate.model.Restaurant;
 import pro.paullezin.graduate.repository.RestaurantRepository;
 import pro.paullezin.graduate.repository.UserRepository;
@@ -12,6 +13,7 @@ import pro.paullezin.graduate.service.VoteService;
 import pro.paullezin.graduate.to.RestaurantTo;
 import pro.paullezin.graduate.web.SecurityUtil;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static pro.paullezin.graduate.util.RestaurantUtil.getAdminTos;
@@ -19,7 +21,8 @@ import static pro.paullezin.graduate.util.RestaurantUtil.getUserTos;
 import static pro.paullezin.graduate.util.ValidationUtil.*;
 import static pro.paullezin.graduate.web.SecurityUtil.authUserIsAdmin;
 
-@Controller
+@RestController
+@RequestMapping(value = "/api/restaurants", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestaurantRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -35,11 +38,13 @@ public class RestaurantRestController {
         this.voteService = voteService;
     }
 
-    public Restaurant get(int id) {
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
         log.info("get restaurant with id {}", id);
         return checkNotFoundWithId(repository.get(id), id);
     }
 
+    @GetMapping
     public List<RestaurantTo> getAll() {
         int userId = SecurityUtil.authUserId();
         log.info("getAll restaurants for user {}", userId);
@@ -48,14 +53,18 @@ public class RestaurantRestController {
         return authUserIsAdmin(userRepository.get(userId)) ? getAdminTos(restaurants) : getUserTos(restaurants);
     }
 
-    public void delete(int id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
         log.info("delete restaurant with id {}", id);
         int userId = SecurityUtil.authUserId();
         assureAdmin(userRepository.get(userId));
         checkNotFoundWithId(repository.delete(id), id);
     }
 
-    public void update(Restaurant restaurant, int id) {
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Restaurant restaurant, @PathVariable int id) {
         log.info("update restaurant {} with id={}", restaurant, id);
         Assert.notNull(restaurant, "restaurant must not be null");
         int userId = SecurityUtil.authUserId();
@@ -64,7 +73,9 @@ public class RestaurantRestController {
         checkNotFoundWithId(repository.save(restaurant), restaurant.getId());
     }
 
-    public Restaurant create(Restaurant restaurant) {
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Restaurant create(@Valid @RequestBody Restaurant restaurant) {
         log.info("create restaurant {}", restaurant);
         Assert.notNull(restaurant, "restaurant must not be null");
         int userId = SecurityUtil.authUserId();
